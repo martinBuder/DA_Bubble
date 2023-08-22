a85f43b3c7180247e0ec3ce27505955db7ff6d98
Albert <wissigkeit@outlook.de>
2023-08-22 12:00:55 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'dabubble-2023',
+    appId: '1:447958867926:web:3a5f7d29cdd2efc6268e77',
+    storageBucket: 'dabubble-2023.appspot.com',
+    apiKey: 'AIzaSyByy3FbokzG52UEBW4KE2qU7JoqOXzKPQg',
+    authDomain: 'dabubble-2023.firebaseapp.com',
+    messagingSenderId: '447958867926',
+  },
+};
