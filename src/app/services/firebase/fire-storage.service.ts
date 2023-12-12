64790e11c375347cc4158aa2cb2738c5ab028b1f
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-12-12 11:17:26 +0100

    get the img to firebase Storage works -- in the folder avatarUpload with an generated token as name

--- src/app/services/firebase/fire-storage.service.ts
+++ src/app/services/firebase/fire-storage.service.ts
@@ -3,6 +3,7 @@ import { FirebaseApp, initializeApp } from '@angular/fire/app';
 import { Storage, getStorage, ref, uploadBytesResumable } from '@angular/fire/storage';
 
 import { environment } from 'src/environments/environment';
+import { FireAuthService } from './fire-auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -19,13 +20,22 @@ export class FireStorageService {
   firebaseApp = initializeApp(environment.firebase);   
 
 
-  
+  generateRandomToken(length: number) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        token += characters.charAt(randomIndex);
+    }
+    return token;
+}
   
   uploadFile(image: File) {    
+    const userImgToken = this.generateRandomToken(32);
     this.storage = getStorage(this.firebaseApp)
     console.log(`next step`);
     
-    const storageFireRef = ref(this.storage);
+    const storageFireRef = ref(this.storage, `gs://mb-dabubble-1985.appspot.com/avatarUpload/${ userImgToken }`);
     uploadBytesResumable(storageFireRef, image);
   } 
       
